2b208f0f4f46c3a935e38ede3ce66f47d65c8f64
hari95159032 <126045869+hari95159032@users.noreply.github.com>
2023-05-23 16:52:41 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,37 @@
+function func(){
+    alert("I will contact you soon!");
+
+}
+
+function Java(){
+
+
+    window.open("https://www.javatpoint.com/java-tutorial")
+
+
+}
+function python(){
+   
+    
+    window.open("https://www.python.org/")
+    
+      
+}
+function html(){
+        
+    window.open("https://www.w3schools.com/html/")
+        
+}
+function css(){
+            
+    window.open("https://www.w3schools.com/css/")
+            
+               
+}
+ 
+function javascript(){
+            
+    window.open("https://javascript.info/")
+            
+               
+}
